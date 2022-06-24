5ca0ed601970e3830937f6f55982a02c30c3a260
binyamdawit <78832827+binyamdawit@users.noreply.github.com>
2022-06-24 16:37:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import BackgroundAnimation from './BackgroundAnimation'
+import InputShortener from './InputShortener'
+import LinkedResult from './LinkedResult'
 
 function App() {
+
+  const [ inputValue, setInputValue ] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <InputShortener setInputValue={setInputValue}/>
+      <BackgroundAnimation />
+      <LinkedResult inputValue={inputValue} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/BackgroundAnimation.js
@@ -0,0 +1,19 @@
+
+const BackgroundAnimation = () => {
+  return (
+    <ul className="background">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+   </ul>
+  )
+}
+
+export default BackgroundAnimation
